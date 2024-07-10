8871a51bdb018cda599fac5a2b841c5984120244
gmsahu <gmanjari77@gmail.com>
2024-07-10 00:45:51 -0600

    model training code added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.cnnClassifier import logger
 
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 # from src.cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
@@ -26,6 +26,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
